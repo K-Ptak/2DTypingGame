5f97523596b2cc86082af70ef4e38cbc0dc739ea
Kamil Ptak <kamil2001p@gmail.com>
2024-03-10 16:37:59 +0100

    Rename and test positioning

--- Player.gd
+++ Player.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	#print(position.x, position.y) <- Current position of player
 
 func _physics_process(_delta):
 	get_input()
